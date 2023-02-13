f3ecb8b9096d19c8bfb57c450cfbe2b3a7791bc0
bartoszkoziel <83342505+bartoszkoziel@users.noreply.github.com>
2023-02-13 12:41:24 +0100

    Add files via upload

--- zuzel/zuzel.js
+++ zuzel/zuzel.js
@@ -38,20 +38,49 @@ class Player {
     constructor(startX, startY) {
         this.x = startX
         this.y = startY
-        this.facing = 0
+        this.speed = 5
+        this.alpha = 0
+        this.vector = {
+            x: this.speed,
+            y: 0
+        }
+        this.skretnosc = 2
 
         this.playerContext = canvas1.getContext("2d")
         this.playerContext.beginPath()
         this.playerContext.lineWidth = 10;
         this.draw()
+
+        document.onkeydown = (e) => {
+            if (e.key == "ArrowDown") {
+                this.alpha = this.alpha - this.skretnosc
+                this.vectorCalc()
+                console.log(this.alpha)
+                console.log(this.vector)
+            }
+            if (e.key == "ArrowUp") {
+                this.alpha = this.alpha + this.skretnosc
+                this.vectorCalc()
+                console.log(this.alpha)
+                console.log(this.vector)
+            }
+        }
     }
 
     draw() {
-        this.playerContext.moveTo(this.x, this.y)
-        this.playerContext.lineTo(this.x + 5, this.y)
+        // this.playerContext.moveTo(this.x, this.y)
+        // this.playerContext.lineTo(this.x + 5, this.y)
+        this.playerContext.arc(this.x, this.y, 3, 0, 2 * Math.PI, true);
         this.playerContext.stroke()
     }
 
+    vectorCalc() {
+        let x = Math.round(Math.cos(this.alpha * Math.PI / 180) * this.speed * 100) / 100
+        let y = Math.round(Math.sin(this.alpha * Math.PI / 180) * this.speed * 100) / 100
+
+        this.vector.x = x
+        this.vector.y = y
+    }
 }
 
-const morot1 = new Player(0, 0)
+const morot1 = new Player(50, 20)
